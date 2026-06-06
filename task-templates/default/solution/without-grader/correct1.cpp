932786f3217dcbf255f1c28f4cf68f6e6c4af2a6
tobiichi3227 <cz1346219@gmail.com>
2026-06-06 10:49:04 +0800

    Add I/O optimization in default cpp solution code

--- task-templates/default/solution/without-grader/correct1.cpp
+++ task-templates/default/solution/without-grader/correct1.cpp
@@ -1,9 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
 	int n;
 	cin >> n;
 	cout << 0 << endl;
+
 	return 0;
 }
